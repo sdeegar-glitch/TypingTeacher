d65cf4eb3050dedeb85c2c855664e96f2b0fa581
sdeegar-glitch <sdeegar@gmail.com>
2026-09-09 10:08:24 +0530

    Updates

---
+++ assets/ScrollableRegion-BwfPYA3t.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-CnSBKPes.js";var t=e();function n({children:e,label:n,className:r=``,axis:i=`x`}){return(0,t.jsx)(`div`,{tabIndex:0,role:`region`,"aria-label":n,className:`${i===`x`?`overflow-x-auto`:`overflow-y-auto`} ${r}`.trim(),children:e})}export{n as t};
